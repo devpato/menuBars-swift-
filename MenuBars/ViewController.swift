d43548d33bb68f1ea4e4a58d69b5e6da9157dba9
Patricio Vargas <patricio_vargasgonzalez@csx.com>
2018-06-29 21:03:02 -0400

    added butons

--- MenuBars/ViewController.swift
+++ MenuBars/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func camaraPress(_ sender: Any) {
+        print("camera press")
+    }
+    
 }
 

7688035dc8004279c0e73b408f5c24cffe015098
Patricio Vargas <patricio_vargasgonzalez@csx.com>
2018-06-30 09:59:47 -0400

    global variable

--- MenuBars/ViewController.swift
+++ MenuBars/ViewController.swift
@@ -9,11 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+     var timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        var timer = Timer()
+       
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: Selector(("processTimer")), userInfo: nil, repeats: true)
     }
 
